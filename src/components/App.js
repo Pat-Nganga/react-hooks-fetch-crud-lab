c0ae305b99a9868eabdcf0fd6ee3ed34ba01da39
Patricia <wambuipatricia3@gmail.com>
2023-04-25 14:13:50 +0300

    Completed Assignments

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,49 @@
-import React, { useState } from "react";
-import AdminNavBar from "./AdminNavBar";
-import QuestionForm from "./QuestionForm";
-import QuestionList from "./QuestionList";
+import React, { useState, useEffect } from 'react'
+import AdminNavBar from './AdminNavBar'
+import QuestionForm from './QuestionForm'
+import QuestionList from './QuestionList'
 
 function App() {
-  const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+  const [page, setPage] = useState('List')
+  function fetchQuestions() {
+    fetch('http://localhost:4000/questions')
+      .then((res) => res.json())
+      .then((data) => setQuestions(data))
+      .catch((error) => console.log(error))
+  }
+  useEffect(() => {
+    fetchQuestions()
+  }, [])
+
+  const addQuestion = (newQuestion) => {
+    fetch('http://localhost:4000/questions', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newQuestion),
+    })
+      .then((res) => res.json())
+      .then((data) => setQuestions([...questions, data]))
+      .catch((error) => console.log(error))
+  }
+  function onDelete() {
+    fetchQuestions()
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === 'Form' ? (
+        <QuestionForm addQuestion={addQuestion} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          setQuestions={setQuestions}
+          onDelete={onDelete}
+        />
+      )}
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React from 'react'
 
-function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+function QuestionItem({ question, onDelete }) {
+  const { id, prompt, answers, correctIndex } = question
 
+  function handleDelete(e) {
+    e.preventDefault()
+    fetch(`http://localhost:4000/questions/${question.id}`, {
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then((data) => onDelete(data))
+  }
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
       {answer}
     </option>
-  ));
+  ))
 
   return (
     <li>
@@ -17,9 +25,9 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
-  );
+  )
 }
 
-export default QuestionItem;
+export default QuestionItem
